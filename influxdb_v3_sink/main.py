4329d00b980f229618e89a2e535eb35f4696fe33
bar roz <barrozz@gmail.com>
2025-10-23 10:24:59 +0000

    Edited 'main.py'

--- influxdb_v3_sink/main.py
+++ influxdb_v3_sink/main.py
@@ -92,7 +92,7 @@ def send_data_to_influx(message):
             "time": writetime
         }
 
-        logger.info(f"MEASUREMENT: {measurement_name},TAGS: {tags}, FIELDS: {fields}, TIME: {writetime}")
+        logger.info(f"MEASUREMENT: {measurement_name}, TAGS: {tags}, FIELDS: {fields}, TIME: {writetime}")
 
         influxdb3_client.write(record=points, write_precision="ms")
         
